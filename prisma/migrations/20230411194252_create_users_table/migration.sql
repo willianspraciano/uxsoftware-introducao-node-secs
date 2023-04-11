e6c358da1b67c9cdbfc3e441dc030640a533f4d0
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-04-11 16:50:18 -0300

    fix: fix database user schema

--- prisma/migrations/20230411194252_create_users_table/migration.sql
+++ prisma/migrations/20230411194252_create_users_table/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "users" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
     "name" TEXT
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
