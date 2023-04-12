51c7a3f4affda26aab7fd8533a0b9d5429d38163
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-04-12 08:41:02 -0300

    feat: create users CRUD

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,10 +4,75 @@ import { PrismaClient } from '@prisma/client';
 export class UsersController {
   public async list(request: Request, response: Response) {
     const prisma = new PrismaClient();
+
     const users = await prisma.user.findMany();
 
     return response.status(200).json({
       users,
+    });
+  }
+
+  public async create(request: Request, response: Response) {
+    const prisma = new PrismaClient();
+
+    const { name, email } = request.body;
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+
+    return response.status(200).json({
+      user,
+    });
+  }
+
+  public async show(request: Request, response: Response) {
+    const prisma = new PrismaClient();
+
+    const { id } = request.params;
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    return response.status(200).json({
+      user,
+    });
+  }
+
+  public async delete(request: Request, response: Response) {
+    const prisma = new PrismaClient();
+
+    const { id } = request.params;
+
+    await prisma.user.delete({
+      where: { id },
+    });
+
+    return response.status(204).json();
+  }
+
+  public async update(request: Request, response: Response) {
+    const prisma = new PrismaClient();
+
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    const userUpdated = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name: String(name),
+        email: String(email),
+      },
+    });
+
+    return response.status(200).json({
+      user: userUpdated,
     });
   }
 }
--- src/routes/UsersRoutes.ts
+++ src/routes/UsersRoutes.ts
@@ -6,5 +6,9 @@ const usersRoutes = Router();
 const controller = new UsersController();
 
 usersRoutes.get('/list', controller.list);
+usersRoutes.post('/create', controller.create);
+usersRoutes.get('/show/:id', controller.show);
+usersRoutes.delete('/delete/:id', controller.delete);
+usersRoutes.put('/update/:id', controller.update);
 
 export { usersRoutes };
